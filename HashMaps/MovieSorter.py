302f4604ec19fd4cef1bc3af7dc2f4a473c2eff9
Taku <132635925+zhira03@users.noreply.github.com>
2024-12-31 12:46:31 +0200

    Add MovieSorter module with routes for adding, listing, searching, and deleting movies

---
+++ HashMaps/MovieSorter.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, jsonify, render_template,redirect,url_for
+import NewHashMap as mapper
+
+app = Flask(__name__)
+
+movieBank = mapper.NewHashMap()
+
+@app.route('/')
+def home():
+    return render_template('index.html')    
+
+@app.route('/add', methods=['POST'])
+def add_movie():
+    if request.method == "POST":
+        title = request.form['title']
+
+        if title:
+            movieBank[title] = "Stored"
+            return redirect(url_for('listed_movies'))
+        return redirect(url_for('add_movie.html'))
+    
+@app.route('/list')
+def listed_movies():
+    movie_list = list(movieBank)
+    return render_template('listed_movies.html', movieBank=movie_list)
+
+@app.route("/search", methods=["POST", "GET"])
+def search_movie():
+    result = None
+
+    while request.method == "POST":
+        title = request.form['title']
+        if title and movieBank.contains(title):
+            result = f"'{title}' is in the collection."
+        else:
+            result = f"'{title}' is not in the collection."
+    return render_template('search_movie.html', result=result)
+
+@app.route('/delete', methods=['POST'])
+def delete_movie():
+    result = None
+
+    while request.method == "POST":
+        title = request.form['title']
+        if title and movieBank.contains(title):
+            movieBank.__delitem__(title)
+            result = f"'{title}' has been deleted."
+        else:
+            result = f"'{title}' is not in the collection."
+    return render_template('delete_movie.html', result=result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
